0557cc86afd824805d1d07a7a003b445286ac5f6
Kelton Lima <kelton.lima@ragt.com.br>
2026-04-14 21:17:42 -0300

    feat(config): implementar configurações de conexão para postgres e redis

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,58 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import computed_field
 
 class Settings(BaseSettings):
+
+    PROJECT_NAME: str = "Video Compressor API"
+
+    VERSION: str = "1.0.0"
+
+    API_V1_STR: str = "/api/v1"
+
+    
+
+    # Declarar os campos para que o Pydantic os leia do ambiente
+
+    POSTGRES_USER: str
+
+    POSTGRES_PASSWORD: str
+
+    POSTGRES_DB: str
+
+    POSTGRES_HOST: str = "db" # Valor padrão
+
+    POSTGRES_PORT: int = 5432
+
+    
+
+    REDIS_HOST: str = "redis"  # Declarar aqui
+
+    REDIS_PORT: int = 6379    # Declarar aqui
+
+    
+
+    @computed_field
+
+    @property
+
+    def DATABASE_URL(self) -> str:
+
+        # Use self.POSTGRES_HOST para respeitar a Opção B ou a var de ambiente
+
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+
+
+    @computed_field
+
+    @property
+
+    def REDIS_URL(self) -> str:
+
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+
+
+
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True, extra="ignore")
     # Variáveis brutas do .env
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
